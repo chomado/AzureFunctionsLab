d47829b219c5d91d29fa1a1d6b6ec723b522c401
chomado <chomado@gmail.com>
2017-10-30 17:16:57 +0900

    Test function

--- AzureFunctionsLab/Functions.cs
+++ AzureFunctionsLab/Functions.cs
@@ -11,8 +11,19 @@ namespace AzureFunctionsLab
 {
     public class Functions
     {
-        public Functions()
+        [FunctionName("Add")] // このメソッドを Azure Function としてマークする FunctionName 属性. 実際のメソッド名自体と一致する必要は無い
+        public static int Add(
+            // このメソッドが HTTP リクエストによって呼び出されるを示します。
+            // このメソッドのパスはデフォルトで /api/Add になります。
+            [HttpTrigger(authLevel: AuthorizationLevel.Function, methods: "get", Route = null)]
+            HttpRequestMessage req,
+            // 診断とエラーのメッセージを記録するために使用できる TraceWriter 
+            TraceWriter log
+        )
         {
+            int x = 1;
+            int y = 2;
+            return x + y;
         }
     }
 }
