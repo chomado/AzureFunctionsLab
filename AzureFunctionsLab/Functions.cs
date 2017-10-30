be4dd091bc55743a1ad53713c8a0e85851a0863f
chomado <chomado@gmail.com>
2017-10-30 14:40:02 +0900

    add an empty Function class

---
+++ AzureFunctionsLab/Functions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureFunctionsLab
+{
+    public class Functions
+    {
+        public Functions()
+        {
+        }
+    }
+}
